04fe91776372aa37dceb4deabfbfefca126fd27a
Joshua Vasquez <joshua.vasquez@alleninstitute.org>
2023-12-12 21:43:37 -0800

    remove harp core

--- README.md
+++ README.md
@@ -1,13 +1,8 @@
-An RP2040-based Harp Hardware/Firmware project template.
+device that accepts Harp timestamps and emits a serial timestamp for ephys data alignment.
 
 ## Features
-* Schematic template for an isolated full speed USB Harp Device with an audio jack
-* basic PCB Layout with size hints for various existing enclosures
-  * DC Barrel Jack (2.1 x 5.5mm, positive center)
-  * USBC
-  * 3.5mm audio jack for Harp synchronization signal
-  * Ground lug
-* firmware template using the [harp.core.rp2040](https://github.com/AllenNeuralDynamics/harp.core.rp2040) library as a submodule
+* uses the [harp.core.rp2040](https://github.com/AllenNeuralDynamics/harp.core.rp2040/tree/main) synchronizer only to synchronize against an external Harp synchronization input.
+* emits a 1000Bps, little-endian, LSbit-first, 32-bit unsigned integer indicating the current time in seconds.
 
 ## Wiring Diagram
 
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,83 +1,40 @@
 #include <cstring>
-#include <harp_c_app.h>
 #include <harp_synchronizer.h>
 #include <core_registers.h>
 #include <reg_types.h>
 #include <config.h>
+#include <pico/divider.h>
 #ifdef DEBUG
     #include <pico/stdlib.h> // for uart printing
     #include <cstdio> // for printf
 #endif
 
-
-// Create device name array.
-const uint16_t who_am_i = 1234;
-const uint8_t hw_version_major = 0;
-const uint8_t hw_version_minor = 0;
-const uint8_t assembly_version = 0;
-const uint8_t harp_version_major = 0;
-const uint8_t harp_version_minor = 0;
-const uint8_t fw_version_major = 0;
-const uint8_t fw_version_minor = 0;
-const uint16_t serial_number = 0xCAFE;
-
-// Harp App Register Setup.
-const size_t reg_count = 2;
-
-// Define register contents.
-#pragma pack(push, 1)
-struct app_regs_t
-{
-    volatile uint8_t test_byte;  // app register 0
-    volatile uint32_t test_uint; // app register 1
-} app_regs;
-#pragma pack(pop)
-
-// Define register "specs."
-RegSpecs app_reg_specs[reg_count]
-{
-    {(uint8_t*)&app_regs.test_byte, sizeof(app_regs.test_byte), U8},
-    {(uint8_t*)&app_regs.test_uint, sizeof(app_regs.test_uint), U32}
-};
-
-// Define register read-and-write handler functions.
-RegFnPair reg_handler_fns[reg_count]
-{
-    {&HarpCore::read_reg_generic, &HarpCore::write_reg_generic},
-    {&HarpCore::read_reg_generic, &HarpCore::write_to_read_only_reg_error}
-};
-
-void app_reset(){}
-
-void update_app_state(){}
-
-// Create Harp App.
-HarpCApp& app = HarpCApp::init(who_am_i, hw_version_major, hw_version_minor,
-                               assembly_version,
-                               harp_version_major, harp_version_minor,
-                               fw_version_major, fw_version_minor,
-                               serial_number, "Example C App",
-                               &app_regs, app_reg_specs,
-                               reg_handler_fns, reg_count, update_app_state,
-                               app_reset);
-
-
+/**
+ * \brief Callback function to dispatch the current timestamp over UART and
+ *  reschedule this function on the next second. Fires when the set alarm time
+ *  takes place.
+ * \details this function will reschedule itself to fire on the next whole
+ *  second (in "Harp" time). Note that Harp time can change slightly upon every
+ *  resynchronization pulse, so the future alarm time is always recomputed
+ *  rather than set to fire on a fixed interval.
+ */
 int64_t dispatch_seconds_from_uart_cb(alarm_id_t id, void* user_data)
 {
-    // We reschedule the message every time references the current harp time
-    // in case the external synchronizer has adjusted the time.
-    uint64_t harp_time_us = app.harp_time_us_64();
-    uint32_t harp_time_s = uint32_t(harp_time_us/1'000'000ULL); // truncate ok.
+    uint64_t harp_time_us = HarpSynchronizer::time_us_64();
+    // Truncate since spec only asks for a 4-byte representation of seconds.
+    uint32_t harp_time_s = uint32_t(div_u64u64(harp_time_us, 1'000'000ULL));
     // The following works because it's little-endian.
     uart_write_blocking(uart0, (uint8_t*)&harp_time_s, sizeof(harp_time_s));
-    // Note: above statement will block and slow down the update loop.
-    //  If this is a dealbreaker, we can use DMA to create a nonblocking write.
+    // Note: above statement will block and slow down the update loop,
+    //  but this should not be an issue since we don't need to poll for
+    //  incoming Harp messages.
     // Reschedule:
     // Get next callback trigger time in pico time on the next whole second
     // in pico time.
-    uint32_t alarm_time_us =
-        HarpCore::harp_to_system_us(harp_time_s*1'000'000UL) + 1'000'000UL;
-    // reschedule this function.
+    uint64_t alarm_time_us =
+        HarpSynchronizer::harp_to_system_us_64(harp_time_s*1'000'000ULL)
+        + 1'000'000ULL;
+    // Reschedule this function.
     add_alarm_at(alarm_time_us, dispatch_seconds_from_uart_cb, nullptr, true);
     return 0; // indicate to not reschedule the alarm because we do so manually.
 }
@@ -88,7 +45,6 @@ int main()
 {
 // Init Synchronizer.
     HarpSynchronizer::init(uart1, HARP_SYNC_RX_PIN);
-    app.set_synchronizer(&HarpSynchronizer::instance());
 #ifdef DEBUG
     stdio_uart_init_full(uart0, 921600, UART_TX_PIN, -1); // use uart1 tx only.
     printf("Hello, from an RP2040!\r\n");
@@ -96,17 +52,19 @@ int main()
     uart_init(uart0, EPHYS_UART_SYNC_BAUDRATE);
     uart_set_format(uart0, 32, 1, UART_PARITY_NONE);
     // Wait for at least one sync signal.
-    while (!HarpSynchronizer::instance().has_synced()){}
+    while (!HarpSynchronizer::has_synced()){}
     // Schedule first callback on the next whole second (in harp time).
-    uint64_t harp_time_us = app.harp_time_us_64();
+    uint64_t harp_time_us = HarpSynchronizer::time_us_64();
     // Round up to next second in harp time.
-    uint32_t remainder;
-    uint32_t quotient = divmod_u32u32_rem(harp_time_us, 1'000'000UL, &remainder);
-    uint32_t alarm_harp_time_us = harp_time_us + 1'000'000UL - remainder;
+    uint64_t remainder;
+    uint64_t quotient = divmod_u64u64_rem(harp_time_us, 1'000'000ULL,
+                                          &remainder);
+    uint64_t alarm_harp_time_us = harp_time_us + 1'000'000ULL - remainder;
     // Convert to pico time.
-    uint32_t alarm_time_us = HarpCore::harp_to_system_us(alarm_harp_time_us);
+    uint64_t alarm_time_us =
+        HarpSynchronizer::harp_to_system_us_64(alarm_harp_time_us);
     // Schedule the first uart dispatch, which will schedule all future ones.
     add_alarm_at(alarm_time_us, dispatch_seconds_from_uart_cb, nullptr, true);
     while(true)
-        app.run();
+    {}
 }
